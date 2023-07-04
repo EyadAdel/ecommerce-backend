ae10ec6ef4f0fb70568c27e0a2bbd519cc4b982a
Eyad <91131512+EyadAdel@users.noreply.github.com>
2023-07-05 00:27:50 +0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ mongoose
     console.log(err);
   });
 
-app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api/auth", authRoute);
